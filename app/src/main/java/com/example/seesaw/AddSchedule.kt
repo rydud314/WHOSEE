b9db12c7b021e78cefc9ae06465d62654757ab13
gyoyeong <rydud314@gmail.com>
2024-09-06 21:35:58 +0900

    0906 merge complete

--- app/src/main/java/com/example/seesaw/AddSchedule.kt
+++ app/src/main/java/com/example/seesaw/AddSchedule.kt
@@ -1,8 +1,6 @@
 package com.example.seesaw
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class AddSchedule : AppCompatActivity() {
