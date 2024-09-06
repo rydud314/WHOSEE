e35caf1cac72c4fbc5a05d525266ff2aa1345a83
gyoyeong <rydud314@gmail.com>
2024-09-06 21:28:26 +0900

    0905 아랑6나연 머지 완료

--- app/src/main/java/com/example/seesaw/Add_Schedule.kt
+++ app/src/main/java/com/example/seesaw/Add_Schedule.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class Add_Schedule : AppCompatActivity() {
+class AddSchedule : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activty_add_schedule)
