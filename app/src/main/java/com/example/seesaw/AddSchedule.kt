c5ecfe5ec56d449b1c4ee95d7ffeb704ff80ce61
gyoyeong <rydud314@gmail.com>
2024-10-23 04:47:26 +0900

    231023 캘린더리스트 가져오기

--- app/src/main/java/com/example/seesaw/AddSchedule.kt
+++ app/src/main/java/com/example/seesaw/AddSchedule.kt
@@ -1,12 +1,16 @@
 package com.example.seesaw
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class AddSchedule : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activty_add_schedule)
+        setContentView(R.layout.activity_add_schedule)
 
+        Log.d(ContentValues.TAG, "캘린더 스케줄 추가 = 입장")
+        
     }
 }
