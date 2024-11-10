957cc8edd45a12ee4fefb517b5ab9553dd3d3ba2
hessepark <hessepark@naver.com>
2024-11-11 00:03:56 +0900

    2024--11-11 chat gpt api

--- app/src/main/java/com/example/seesaw/model/ChatGPTApi.kt
+++ app/src/main/java/com/example/seesaw/model/ChatGPTApi.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 interface ChatGPTApi {
     @Headers(
         "Content-Type: application/json",
-        "Authorization: Bearer " // api키는 Bearer 뒤에 공백 한칸 띄고 입력합니다.
+        "Authorization: Bearer " // api키는 Bearer 뒤에 공백 한칸 띄고 입력.
     )
     @POST("v1/chat/completions")
     fun getChatResponse(@Body request: ChatGPTRequest?): Call<ChatGPTResponse?>?
